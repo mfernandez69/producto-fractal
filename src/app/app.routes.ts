b32ce685a41cdcc487107cf96eb9240d12ece1b7
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-05-09 12:04:05 +0200

    feat:creacion servicio de inicio de sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './auth/auth.routes';
 
 export const routes: Routes = [
+  ...authRoutes,
   { path: '', loadComponent: () => import('./features/home/home-page/home-page.component').then(m => m.default) }
 ];
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,66 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  UserCredential,
+  signOut
+} from '@angular/fire/auth';
+import { FirebaseError } from 'firebase/app';
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _auth = inject(Auth);
+
+  // Register a new user with email and password
+  async signUp(email: string, password: string): Promise<UserCredential> {
+    try {
+      return await createUserWithEmailAndPassword(
+        this._auth,
+        email,
+        password
+      );
+    } catch (error) {
+      console.error('Error in signUp:', error);
+      throw error;
+    }
+  }
+
+  // Login with email and password
+  async signIn({ email, password }: LoginData): Promise<UserCredential> {
+    try {
+      return await signInWithEmailAndPassword(this._auth, email, password);
+    } catch (error) {
+      console.error('Error in signIn:', error);
+      if (error instanceof FirebaseError) {
+        throw error;
+      }
+      throw new Error('Unknown authentication error');
+    }
+  }
+
+  // Login with Google
+  signInWithGoogle(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this._auth, provider);
+  }
+
+  // Logout
+  signOut(): Promise<void> {
+    return signOut(this._auth);
+  }
+
+  // Get current user
+  get currentUser() {
+    return this._auth.currentUser;
+  }
+}
