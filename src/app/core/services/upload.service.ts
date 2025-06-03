d160996d62c63f5c15dabf7a060397c4badfdd96
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-06-03 16:05:20 +0200

    feat:claveuploadthing

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../../environment';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
